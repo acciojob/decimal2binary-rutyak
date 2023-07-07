5208a9f97f9a2b03859cbbf7e995e7da5e743c82
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:07:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-function decimalToBinary(decimal) {
-  // Check if the input is a valid positive integer
-  if (!Number.isInteger(decimal) || decimal < 0) {
-    console.log('Input must be a positive integer.');
-    return;
-  }
-
-  // Base case: decimal number is 0
-  if (decimal === 0) {
-    return '0';
-  }
-
-  let binary = '';
-
-  // Convert decimal to binary using repeated division by 2
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-// Test case
-let a = prompt();
-const decimal = a.parseInt();
-const binary = decimalToBinary(decimal);
-console.log(binary); // Output: 111
-
-
+window.decimalToBinary = decimalToBinary;
